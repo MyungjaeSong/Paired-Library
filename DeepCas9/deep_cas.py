70e6cc02ca94cfc5b0c63b1865a73f524eb96c11
NRGENE\doronst <doronst@nrgene.com>
2020-06-30 15:20:38 +0300

    refactor parameters, add argparser, r=0.77 (fails during plot)

--- DeepCas9/deep_cas.py
+++ DeepCas9/deep_cas.py
@@ -1,6 +1,6 @@
+import argparse
+import sys
 from os.path import dirname
-
-import numpy as np
 import pandas as pd
 import scipy.stats
 import tensorflow as tf
@@ -9,9 +9,34 @@ from tensorflow.python.keras import regularizers
 import tensorflow_probability as tfp
 from DeepCas9.utils import *
 
+# BUFFER_SIZE = int(1e4)
+# BATCH_SIZE = 500
+
+sequence_column = 'Target context sequence (4+20+3+3)'
+frequency_column = 'Background subtracted indel (%)'
 
 
-def build_model():
+def parse_args(argv):
+    parser = argparse.ArgumentParser(prog='DeepSpCas9',
+                                     description='tensorflow2 implementation (similar to) DeepSpCas9 method')
+    parser.add_argument('-training_file', default=dirname(__file__) + '/dataset/training.tsv')
+    parser.add_argument('-testing_file', default=dirname(__file__) + '/dataset/endogenous_sites_test_set.tsv')
+    parser.add_argument('-model_file', default=dirname(__file__) + '/model')
+    parser.add_argument('--do_training', action='store_true', default=False,
+                        help='train the model from training data (otherwise load trained model from -model_file)')
+    parser.add_argument('-epochs', type=int, default=20)
+    parser.add_argument('-validation_frac', type=float, default=0.1,
+                        help='fraction of training set to leave for validation')
+    parser.add_argument('-filter_size', type=int, default=7,
+                        help='number of consecutive bp to learn local features from')
+    parser.add_argument('-num_of_filters', type=int, default=50, help='number of filters in convolutional layer')
+    parser.add_argument('-num_of_dense_layer_neurons', type=int, default=64, help='number of nuerons in dense layer')
+    parser.add_argument('-dropout', type=float, default=0.2, help='dropout fraction')
+
+    return parser.parse_args(argv[1:])
+
+
+def build_model(filter_size, num_of_filters, num_of_dense_layer_neurons, dropout_rate):
     lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
         0.001,
         decay_steps=1000,
@@ -20,7 +45,7 @@ def build_model():
 
     model = tf.keras.models.Sequential([
         # Apply filter each consecutive 7bp (7X4 matrix)
-        keras.layers.Conv2D(50, (7,4), activation='relu', input_shape=(30, 4, 1), padding='same',
+        keras.layers.Conv2D(num_of_filters, (filter_size, 4), activation='relu', input_shape=(30, 4, 1), padding='same',
                             kernel_regularizer=regularizers.l2(0.001)),
         # Downsamples the input representation by taking the maximum value over the window defined by pool_size
         # for each dimension along the features axis
@@ -29,8 +54,8 @@ def build_model():
         #                    kernel_regularizer=regularizers.l1(0.001)),
         #keras.layers.MaxPooling2D((2, 1)),
         keras.layers.Flatten(),
-        keras.layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l1(0.001)),
-        keras.layers.Dropout(0.2),
+        keras.layers.Dense(num_of_dense_layer_neurons, activation='relu', kernel_regularizer=regularizers.l1(0.001)),
+        keras.layers.Dropout(dropout_rate),
         keras.layers.Dense(1, activation='relu')
     ])
 
@@ -44,95 +69,77 @@ def build_model():
     return model
 
 
-def sequence_to_one_hot(sequence):
-    length = 30
-    one_hot_encoded_sequence = np.zeros((length, 4), dtype=int)
-    for i in range(length):
-        if sequence[i] in "Aa":
-            one_hot_encoded_sequence[i, 0] = 1
-        elif sequence[i] in "Cc":
-            one_hot_encoded_sequence[i, 1] = 1
-        elif sequence[i] in "Gg":
-            one_hot_encoded_sequence[i, 2] = 1
-        elif sequence[i] in "Tt":
-            one_hot_encoded_sequence[i, 3] = 1
-        else:
-            raise RuntimeError("Non-ATGC character " + sequence)
-    return one_hot_encoded_sequence
-
-
-def pack_row(*row):
-    label = row[0]
-    features = tf.stack(row[1:], 1)
-    return features, label
-
-
 def main():
-    EPOCHS = 20
-    BUFFER_SIZE = int(1e4)
-    BATCH_SIZE = 500
+    args = parse_args(sys.argv)
+    training_file = args.training_file
+    test_data_file = args.testing_file
+    model_file = args.model_file
+    do_training = args.do_training
+    epochs = args.epochs
+    validation_fraction = args.validation_frac
+    filter_size = args.filter_size
+    num_of_filters = args.num_of_filters
+    num_of_dense_layer_neurons = args.num_of_dense_layer_neurons
+    dropout_rate = args.dropout
+    if do_training:
+        trained_model = train_model(training_file, validation_fraction, filter_size, num_of_filters, num_of_dense_layer_neurons,
+                                dropout_rate, epochs)
+        trained_model.save(model_file)
+    else:
+        trained_model = keras.models.load_model(model_file)
+    test_model(trained_model, test_data_file)
 
-    training_file = dirname(__file__) + '/dataset/training.tsv'
+
+def train_model(training_file, validation_fraction, filter_size, num_of_filters, num_of_dense_layer_neurons, dropout, epochs):
+    # Load data
     training_input_df = pd.read_csv(training_file, sep='\t')
-    training_input_df.sample(frac=1) # shuffle the data
-
-    n_train_and_validation = training_input_df.shape[0]
-    n_validation = int(n_train_and_validation / 10)
-    n_train = n_train_and_validation - n_validation
-    steps_per_epoch = n_train // BATCH_SIZE
-
-    sequence_column = 'Target context sequence (4+20+3+3)'
-    frequency_column = 'Background subtracted indel (%)'
+    # Transform data (to tf compatible inputs)
     print(training_input_df[sequence_column])
-
-    print("Convert sequence into one-hot encoding matrices")
-    onehot_encoded_sequences = training_input_df[sequence_column].apply(sequence_to_one_hot)
-    onehot_encoded_sequences_3d_matrix = np.stack(onehot_encoded_sequences.tolist())
-
     indel_frequencies = training_input_df[frequency_column].to_numpy()
-    #plt.hist(indel_frequencies, 30); plt.show()
-    onehot_encoded_sequences_3d_matrix = np.expand_dims(onehot_encoded_sequences_3d_matrix, axis=3)
+    print("Convert sequence into one-hot encoding matrices")
+    onehot_encoded_sequences_3d_matrix = series_to_onehot_encoding_3d_matrix(training_input_df[sequence_column])
     print(onehot_encoded_sequences_3d_matrix.shape)
     print(indel_frequencies.shape)
-    train_and_validation_dataset = tf.data.Dataset.from_tensor_slices(
-        (onehot_encoded_sequences_3d_matrix, indel_frequencies))
-
-    train_and_validation_dataset.shuffle(BUFFER_SIZE)
-
-    # index = 0
-    # for x, y in train_and_validation_dataset:
-    #     print(index)
-    #     index += 1
-    #     print(x)
-    #     print(y)
-    #     if index > 0:
-    #         break
-
-    train_and_validation_dataset.shuffle(BUFFER_SIZE)
-    validate_ds = train_and_validation_dataset.take(n_validation)
-    train_ds = train_and_validation_dataset.skip(n_validation).take(n_train).cache()
-
+    # shuffle the data
+    training_input_df.sample(frac=1)
+    n_train_and_validation = training_input_df.shape[0]
+    validation_set_size = int(n_train_and_validation * validation_fraction)
+    training_set_size = n_train_and_validation - validation_set_size
+    # plt.hist(indel_frequencies, 30); plt.show()
     # Build the model
-    model = build_model()
-
+    model = build_model(filter_size, num_of_filters, num_of_dense_layer_neurons, dropout)
+    # train the model
     history = model.fit(
-        onehot_encoded_sequences_3d_matrix[2000:],
-        indel_frequencies[2000:],
-        epochs=EPOCHS,
-        validation_data=(onehot_encoded_sequences_3d_matrix[:2000], indel_frequencies[:2000]),
+        onehot_encoded_sequences_3d_matrix[validation_set_size:],
+        indel_frequencies[validation_set_size:],
+        epochs=epochs,
+        validation_data=(
+        onehot_encoded_sequences_3d_matrix[:validation_set_size], indel_frequencies[:validation_set_size]),
         verbose=1)
-
     # predict a subset of the training set (sanity)
-    predicted = [x[0] for x in model.predict(onehot_encoded_sequences_3d_matrix[:2000])]
-    actual = indel_frequencies[:2000]
-
+    predicted = [x[0] for x in model.predict(onehot_encoded_sequences_3d_matrix[:validation_set_size])]
+    actual = indel_frequencies[:validation_set_size]
     # Calculate pearson correlation
     predicted_arr = np.asarray(predicted)
     print(f'pearson correlation: {scipy.stats.pearsonr(predicted_arr, actual)}')
-    #print(f'pearson correlation: {tfp.stats.correlation(predicted_arr, actual)}')
-
-
+    # print(f'pearson correlation: {tfp.stats.correlation(predicted_arr, actual)}')
     visualize_history(history)
+    return model
+
+
+def test_model(trained_model, test_data_file):
+    testing_input_df = pd.read_csv(test_data_file, sep='\t', skiprows=1, columns=[sequence_column, frequency_column], usecols=[0, 1])
+    one_hot_encoded_sequences_3d_matrix = series_to_onehot_encoding_3d_matrix(testing_input_df[sequence_column])
+    predictions = trained_model(one_hot_encoded_sequences_3d_matrix)
+    print(predictions)
+
+
+def series_to_onehot_encoding_3d_matrix(sequences_series):
+    one_hot_encoded_sequences = sequences_series.apply(sequence_to_one_hot)
+    onehot_encoded_sequences_3d_matrix = np.stack(one_hot_encoded_sequences.tolist())
+    onehot_encoded_sequences_3d_matrix = np.expand_dims(onehot_encoded_sequences_3d_matrix, axis=3)
+    return onehot_encoded_sequences_3d_matrix
+
 
 
 if __name__ == "__main__":
--- DeepCas9/utils.py
+++ DeepCas9/utils.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def visualize_history(history):
     training_corr = history.history['correlation']
@@ -15,3 +15,20 @@ def visualize_history(history):
     plt.xlabel('Epoch')
     plt.ylabel('Correlation')
     plt.show()
+
+
+def sequence_to_one_hot(sequence):
+    length = 30
+    one_hot_encoded_sequence = np.zeros((length, 4), dtype=int)
+    for i in range(length):
+        if sequence[i] in "Aa":
+            one_hot_encoded_sequence[i, 0] = 1
+        elif sequence[i] in "Cc":
+            one_hot_encoded_sequence[i, 1] = 1
+        elif sequence[i] in "Gg":
+            one_hot_encoded_sequence[i, 2] = 1
+        elif sequence[i] in "Tt":
+            one_hot_encoded_sequence[i, 3] = 1
+        else:
+            raise RuntimeError("Non-ATGC character " + sequence)
+    return one_hot_encoded_sequence
